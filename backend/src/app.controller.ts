4786b153f1dfbb2de791b539ffc089d5f51e47d3
손서진 <sonseojin702@gmail.com>
2026-07-18 15:24:24 +0900

    feat: user info

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-
-
-// 수강생 김홍엽 작업 영역
-const currentWorker = "으아아아악";
-
+// 수강생 작업 영역 - 머 추가하라고 함.
+const currentWorker = "Biggy smalls real name, no gimmicks";
 
 @Controller()
 export class AppController {
@@ -26,6 +23,3 @@ export class AppController {
     return this.appService.getServerInfo();
   }
 }
-
-//수강생 정재경 작업영역
-const currentWorker = "정재경";
