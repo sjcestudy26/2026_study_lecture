2893c8aef03a5ec6abd528c2ce2db826dc19e53c
YONGJAE CHOI <cyj19797@gmail.com>
2026-07-18 16:27:23 +0900

    git stash

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,16 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+<<<<<<< HEAD
 // 수강생 작업 영역 - 머 추가하라고 함.
 const currentWorker = "Biggy smalls real name, no gimmicks";
+=======
+// 수강생 김홍엽 작업 영역
+const currentWorker = '김홍엽';
+>>>>>>> c0afb7e (git stash)
 
 @Controller()
 export class AppController { // 주석달기
   constructor(private readonly appService: AppService) {}
 
+<<<<<<< HEAD
   
   @Get('health') // 하이
   getHealth() { //머지??
+=======
+  @Get('health') //주석입니다 ㅎㅇ ㅎㅎㅎㅎㅎ
+  getHealth() {
+>>>>>>> c0afb7e (git stash)
     return this.appService.getHealth();
   }
 
@@ -23,6 +33,7 @@ export class AppController { // 주석달기
   @Get('server-info') ///asdf wasd
   //git stash111111
   getServerInfo() {
+    //안녕하세요
     return this.appService.getServerInfo();
   } // TESTTESTESTs
 }
