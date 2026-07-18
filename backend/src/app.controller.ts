ca67abdfbd56033696eddba6a2a094b05d8fe9a4
YONGJAE CHOI <cyj19797@gmail.com>
2026-07-18 17:34:22 +0900

    rollback

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,30 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-<<<<<<< HEAD
-// 수강생 작업 영역 - 머 추가하라고 함.
-const currentWorker = "Biggy smalls real name, no gimmicks";
-=======
 // 수강생 김홍엽 작업 영역
 const currentWorker = '김홍엽';
->>>>>>> c0afb7e (git stash)
 
 @Controller()
-export class AppController { // 주석달기
+export class AppController {
+  // 주석달기
   constructor(private readonly appService: AppService) {}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  
-  @Get('health') // 하이
-  getHealth() { //머지??
-=======
   @Get('health') //주석입니다 ㅎㅇ ㅎㅎㅎㅎㅎ
-=======
-  @Get('health')
->>>>>>> 81bc205 (origin)
   getHealth() {
->>>>>>> c0afb7e (git stash)
     return this.appService.getHealth();
   }
 
@@ -43,7 +29,7 @@ export class AppController { // 주석달기
 }
 
 //수강생 정재경 작업영역
-const currentWorker = "정재경";
+const currentWorker = '정재경';
 
 //sdfdsfs
-const currentWorker1 = "송민준";
+const currentWorker1 = '송민준';
