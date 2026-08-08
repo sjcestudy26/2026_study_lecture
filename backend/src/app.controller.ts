0d2523b9416033b6ce2c2673c8a8748ca712d32d
ennmej <ennmej@naver.com>
2026-08-08 15:29:26 +0900

    오류수정

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -7,17 +7,13 @@ import { AppService } from './app.service';
 // git rebase main -> 코드 수정 -> git add . -> git rebase --continue 
 // -> git log --oneline -> git push origin feature/각자브랜치 --force
 
-// 수강생 김홍엽 작업 영역
-const currentWorker = "으아아아악";
-const currentWorker = "여기는 단풍브랜치요";
-
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('health')
-  getHealth() {//니니게그런사람이나힐순없는지니곁에있는니친구가아니라 //머지??
+  getHealth() {
     return this.appService.getHealth();
   }
 
@@ -26,13 +22,10 @@ export class AppController {
     return this.appService.checkDatabase();
   }
 
-  // stash 후 server-info add comment
-  @Get('server-info') ///asdf wasd //git stash111111 // wow wonderful
+  @Get('server-info') 
   getServerInfo() {
       return this.appService.getServerInfo();
-  } // TESTTESTESTs
+  } 
     
 }
 
-//수강생 정재경 작업영역
-const currentWorker = "정재경";
