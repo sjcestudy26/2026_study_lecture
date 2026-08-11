39f9e8a3b52c8c45611f5a28893afe13f45ecc54
limsg <kilsoo1234@naver.com>
2026-08-11 19:01:26 +0900

    테스트 코드

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -29,7 +29,7 @@ export class AppController {
       return this.appService.getServerInfo();
   } // TESTTESTESTs
 
-  @Get('test')
+  @Get('backend_api/test')
   getTest() {
     return {
       status: 'ok',
